abd345d07adda298c56691a7b323e1b97605c95a
Jay <106968807+ethelbell@users.noreply.github.com>
2024-12-17 13:11:00 +0800

    only close position

--- programs/position/src/lib.rs
+++ programs/position/src/lib.rs
@@ -10,11 +10,7 @@ declare_id!("EvvVHCu51jPjxJv7nHipFcWkCL3PxL22rbGg4B7BYayu");
 pub mod position {
     use super::*;
 
-    pub fn adjust_position(
-        ctx: Context<AdjustPosition>,
-        tick_lower_index: i32,
-        tick_upper_index: i32,
-    ) -> Result<()> {
-        return instructions::adjust_position::handler(ctx, tick_lower_index, tick_upper_index);
+    pub fn adjust_position(ctx: Context<AdjustPosition>) -> Result<()> {
+        return instructions::adjust_position::handler(ctx);
     }
 }
