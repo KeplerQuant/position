fe1944fe3404c8b1b9e4360f92479e0f2dcad98e
Jay <106968807+ethelbell@users.noreply.github.com>
2024-12-26 19:07:12 +0800

    delete whirlpool

--- programs/position/src/instructions/adjust_position.rs
+++ programs/position/src/instructions/adjust_position.rs
@@ -1,52 +1,10 @@
 use anchor_lang::prelude::*;
-use anchor_spl::{
-    token_2022::{self, Token2022},
-    token_interface::{Mint, TokenAccount},
-};
-use whirlpool_cpi::{program::Whirlpool as WhirlpoolProgram, state::Position};
 
 #[derive(Accounts)]
-pub struct AdjustPosition<'info> {
-    pub whirlpool_program: Program<'info, WhirlpoolProgram>,
+pub struct AdjustPosition {}
 
-    pub position_authority: Signer<'info>,
-
-    /// CHECK: safe, for receiving rent only
-    #[account(mut)]
-    pub receiver: UncheckedAccount<'info>,
-
-    #[account(mut)]
-    pub position: Account<'info, Position>,
-
-    #[account(mut, address = position.position_mint, owner = token_2022_program.key())]
-    pub position_mint: InterfaceAccount<'info, Mint>,
-
-    #[account(mut,
-        constraint = position_token_account.amount == 1,
-        constraint = position_token_account.mint == position.position_mint
-    )]
-    pub position_token_account: InterfaceAccount<'info, TokenAccount>,
-
-    #[account(address = token_2022::ID)]
-    pub token_2022_program: Program<'info, Token2022>,
-}
-
-pub fn handler(ctx: Context<AdjustPosition>) -> Result<()> {
-    let cpi_program = ctx.accounts.whirlpool_program.to_account_info();
-
-    let cpi_accounts = whirlpool_cpi::cpi::accounts::ClosePositionWithTokenExtensions {
-        position_authority: ctx.accounts.position_authority.to_account_info(),
-        receiver: ctx.accounts.receiver.to_account_info(),
-        position: ctx.accounts.position.to_account_info(),
-        position_mint: ctx.accounts.position_mint.to_account_info(),
-        position_token_account: ctx.accounts.position_token_account.to_account_info(),
-        token_2022_program: ctx.accounts.token_2022_program.to_account_info(),
-    };
-
-    let cpi_ctx = CpiContext::new(cpi_program.clone(), cpi_accounts);
-
+pub fn handler(_ctx: Context<AdjustPosition>) -> Result<()> {
     msg!("CPI: whirlpool close_position_with_token_extensions instruction");
-    whirlpool_cpi::cpi::close_position_with_token_extensions(cpi_ctx)?;
 
     Ok(())
 }
--- tests/src/test_adjust_position.rs
+++ tests/src/test_adjust_position.rs
@@ -6,8 +6,6 @@ use anchor_client::{
     },
     Client, Cluster,
 };
-use anchor_spl::token_2022::spl_token_2022;
-use orca_whirlpools_client::get_position_address;
 use spl_associated_token_account::get_associated_token_address_with_program_id;
 
 #[test]
@@ -21,7 +19,6 @@ fn test_adjust_position() {
     let program = client.program(program_id).unwrap();
 
     let position_mint = Pubkey::from_str("3sMCGsHSDtihSuuQ8KxX7XrKaGYfc38WE77SELcRNAPK").unwrap();
-    let (position_pda, _) = get_position_address(&position_mint).unwrap();
     let mint_info = program.rpc().get_account(&position_mint).unwrap();
     let position_token_account = get_associated_token_address_with_program_id(
         &program.payer(),
@@ -33,15 +30,7 @@ fn test_adjust_position() {
 
     let tx = program
         .request()
-        .accounts(position::accounts::AdjustPosition {
-            whirlpool_program: whirlpool_cpi::ID,
-            position_authority: program.payer(),
-            receiver: program.payer(),
-            position: position_pda,
-            position_mint,
-            position_token_account,
-            token_2022_program: spl_token_2022::ID,
-        })
+        .accounts(position::accounts::AdjustPosition {})
         .args(position::instruction::AdjustPosition {})
         .send()
         .expect("");
